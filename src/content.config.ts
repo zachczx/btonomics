5f3b107712353d7ec7e2cf04b10f657818e7849a
zachczx <109778851+zachczx@users.noreply.github.com>
2026-07-22 21:11:23 +0800

    chore: upgrade to call zod4 for content definition

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content'
+import { defineCollection } from 'astro:content'
+import { z } from 'astro/zod'
 import { glob } from 'astro/loaders'
 import { fileURLToPath } from 'node:url'
 
@@ -15,6 +16,7 @@ const blog = defineCollection({
             category: z.string(),
             author: z.string(),
             image: image().optional(),
+            coverImagePosition: z.enum(['top', 'center', 'bottom']).optional(),
             tags: z.array(z.string()).optional(),
             slug: z.string().optional(),
             mood: z.string().optional(),
